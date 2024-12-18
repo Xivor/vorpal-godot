fab9a0a816b13c095170720a4be2cefacb7306a7
Francisco Henriques <franciscolealhenriques@usp.br>
2024-12-18 11:50:03 -0300

    Add ModuleInitializationLevel as argument for initialize and uninitialize methods
    Now it is necessary to add a ModuleInitializationLevel (declared in
    modules/register_module_types.h) as an argument to initialize and
    uninitialize module methods.

--- register_types.cxx
+++ register_types.cxx
@@ -12,8 +12,8 @@
 
 #include <iostream>
 #include <memory>
-#include <vector>
 #include <string>
+#include <vector>
 
 namespace vorpal {
 namespace godot {
@@ -24,46 +24,41 @@ using std::vector;
 
 class VORPALModule : public Reference {
   OBJ_TYPE(VORPALModule, Reference);
- public:
+
+public:
   bool start(const String &path) {
     return engine_.start(vector<string>(1, path.ascii().get_data())).ok();
   }
-  void finish() {
-    engine_.finish();
-  }
+  void finish() { engine_.finish(); }
   bool ok() const { return engine_.started(); }
   int eventInstance(const String &name) {
     shared_ptr<vorpal::SoundtrackEvent> event;
-    vorpal::Status status = engine_.eventInstance(name.ascii().get_data(), &event);
+    vorpal::Status status =
+        engine_.eventInstance(name.ascii().get_data(), &event);
     if (status.ok()) {
       events_.push_back(event);
-      return events_.size()-1;
+      return events_.size() - 1;
     }
     std::cout << "[VORPAL-wrap] Failed to instanciate event '"
               << name.ascii().get_data() << "'" << std::endl;
     std::cout << "[VORPAL-wrap] " << status.description() << std::endl;
     return -1;
   }
-  void freeEvent(int id) {
-    events_[id].reset();
-  }
-  void clear() {
-    events_.clear();
-  }
-  void pushCommand (int id, const String &cmd) {
+  void freeEvent(int id) { events_[id].reset(); }
+  void clear() { events_.clear(); }
+  void pushCommand(int id, const String &cmd) {
     events_[id]->pushCommand(cmd.ascii().get_data());
   }
-  void pushCommand1f (int id, const String &cmd, float arg) {
+  void pushCommand1f(int id, const String &cmd, float arg) {
     events_[id]->pushCommand(cmd.ascii().get_data(), arg);
   }
-  void setEventPosition (int id, float x, float y, float z) {
+  void setEventPosition(int id, float x, float y, float z) {
     events_[id]->setAudioSource(x, y, z);
   }
-  void tick (double dt) {
-    engine_.tick(dt);
-  }
- protected:
-  static void _bind_methods () {
+  void tick(double dt) { engine_.tick(dt); }
+
+protected:
+  static void _bind_methods() {
     ObjectTypeDB::bind_method("ok", &VORPALModule::ok);
     ObjectTypeDB::bind_method("start", &VORPALModule::start);
     ObjectTypeDB::bind_method("finish", &VORPALModule::finish);
@@ -76,18 +71,19 @@ class VORPALModule : public Reference {
     ObjectTypeDB::bind_method("push_command_1f", &VORPALModule::pushCommand1f);
     ObjectTypeDB::bind_method("tick", &VORPALModule::tick);
   }
- private:
-  vorpal::Engine                               engine_;
-  vector<shared_ptr<vorpal::SoundtrackEvent>>  events_;
+
+private:
+  vorpal::Engine engine_;
+  vector<shared_ptr<vorpal::SoundtrackEvent>> events_;
 };
 
 } // namespace godot
 } // namespace vorpal
 
-void initialize_vorpal_module () {
+void initialize_vorpal_module(ModuleInitializationLevel p_level) {
   ObjectTypeDB::register_type<vorpal::godot::VORPALModule>();
 }
 
-void uninitialize_vorpal_module () {
+void uninitialize_vorpal_module(ModuleInitializationLevel p_level) {
   // nothing at all?
 }
--- register_types.h
+++ register_types.h
@@ -2,8 +2,9 @@
 #ifndef VORPAL_GODOT_REGISTER_TYPES_H_
 #define VORPAL_GODOT_REGISTER_TYPES_H_
 
-void initialize_vorpal_module ();
-void uninitialize_vorpal_module ();
+#include "modules/register_module_types.h"
+
+void initialize_vorpal_module (ModuleInitializationLevel p_level);
+void uninitialize_vorpal_module (ModuleInitializationLevel p_level);
 
 #endif // VORPAL_GODOT_REGISTER_TYPES_H_
-
